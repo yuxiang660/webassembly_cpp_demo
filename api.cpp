60c6c735adf5729e614bbe142637724920336e97
yuxiang660 <yuxiang660@users.noreply.github.com>
2021-11-03 02:14:05 +0000

    Automated deployment: Wed Nov  3 02:14:05 UTC 2021 ff988e6a1096234cdb6dfbedb95cd8a28b885e5b

---
+++ api.cpp
@@ -0,0 +1,17 @@
+#include "./api.h"
+
+namespace DEMO
+{
+
+int show_me_the_answer()
+{
+   return 42;
+}
+
+float add(float a, float b)
+{
+   return a+b;
+}
+
+}
+
---
+++ web_api.cpp
@@ -0,0 +1,27 @@
+#ifndef EM_PORT_API
+#   if defined(__EMSCRIPTEN__)
+#      include <emscripten.h>
+#      if defined(__cplusplus)
+#         define EM_PORT_API(rettype) extern "C" rettype EMSCRIPTEN_KEEPALIVE
+#      else
+#         define EM_PORT_API(rettype) rettype EMSCRIPTEN_KEEPALIVE
+#      endif
+#   else
+#      if defined(__cplusplus)
+#         define EM_PORT_API(rettype) extern "C" rettype
+#      else
+#         define EM_PORT_API(rettype) rettype
+#      endif
+#   endif
+#endif
+
+#include "./api.h"
+
+EM_PORT_API(int) show_me_the_answer_c() {
+   return DEMO::show_me_the_answer();
+}
+
+EM_PORT_API(float) add_c(float a, float b) {
+   return DEMO::add(a, b);
+}
+
